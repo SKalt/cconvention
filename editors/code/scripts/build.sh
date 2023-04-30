71f08e9f9ac24bd967e1b44246dc55ffddea8f1b
Steven Kalt <kalt.steven@gmail.com>
2023-04-29 23:40:27 -0400

    feat(vscode): add highlighting
    It turns out language servers can't provide highlighting support outside
    of the semantic token service (see https://code.visualstudio.com/api/language-extensions/syntax-highlight-guide#tokenization,
    https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#textDocument_semanticTokens)
    It makes sense to perform tokenization in a fast, sync pass using regex
    (a regular language) instead of a context-dependent/fully-featured parser.

--- editors/code/scripts/build.sh
+++ editors/code/scripts/build.sh
@@ -2,8 +2,10 @@
 # this script assumes that it's being called as part of `npm run`:
 # -../node_modules/.bin/ must be at the front of $PATH
 # - the current working directory must be ..
-esbuild ./src/main.ts --bundle   --outfile=./dist/main.min.common.js \
+esbuild ./src/main.ts --bundle --outfile=./dist/main.min.common.js \
   --format=cjs --platform=node --target=node18 \
   --external:vscode \
   --minify-{whitespace,identifiers,syntax} --sourcemap \
   "$@"
+# TODO: jsonify the grammar
+./node_modules/.bin/js-yaml ./src/tmLanguage.yaml >./src/tmLanguage.json
